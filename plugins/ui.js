0c1113c44c4976e230514802361372725a4096c6
cshaptx4869 <994774638@qq.com>
2024-06-18 14:24:06 +0800

    refactor: :recycle: 修改UI默认配置

--- plugins/ui.js
+++ plugins/ui.js
@@ -8,10 +8,19 @@ export function setupUI(app) {
     // 修改$uv.config对象的属性
     config: {
       // 修改默认单位为rpx，相当于执行 uni.$uv.config.unit = 'rpx'
-      // 在用于传入组件大小参数为数值时，就默认为rpx
-      unit: "rpx",
+      // 用于在传入组件大小参数为数值时，指定默认单位
+      unit: "px",
     },
     // 修改$uv.props对象的属性
-    props: {},
+    props: {
+      // 修改uv-text组件的size参数的默认值，注意：默认值都要用default声明
+      text: {
+        color: {
+          default: "#303133",
+        },
+      },
+      // 其他组件属性配置，具体的参数名称可以去每个组件的props.js中进行查看
+      // ......
+    },
   });
 }
